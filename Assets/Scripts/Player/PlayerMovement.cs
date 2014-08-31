da5039b5d0fd5f828eea5f96d474ee7687850b28
Dan Ruscoe <danruscoe@gmail.com>
2014-08-31 00:03:45 -0700

    Player now follows cursor position.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+	private float speed = 2f;
+	private Vector3 target;
+
 	private int raycastLayerMask;
-	
 	private RaycastHit raycastHit;
 
 	void Awake()
@@ -18,10 +20,20 @@ public class PlayerMovement : MonoBehaviour
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity, this.raycastLayerMask))
+			if (Physics.Raycast(ray, out this.raycastHit, Mathf.Infinity, this.raycastLayerMask))
 			{
-				Debug.Log("Clicked Ground.");
+				Debug.Log("Clicked Ground at point:");
+				Debug.Log(this.raycastHit.point);
+
+				this.target = new Vector3(this.raycastHit.point.x, this.gameObject.transform.position.y, this.raycastHit.point.z);
 			}
+		}
+
+		if ((this.gameObject.transform.position.x != this.target.x)
+		    && (this.gameObject.transform.position.z != this.target.z))
+		{
+			float step = (this.speed * Time.deltaTime);
+			this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.target, step);
 		}
 	}
 }
