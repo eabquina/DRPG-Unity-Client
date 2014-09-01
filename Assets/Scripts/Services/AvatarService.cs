6a9684604bae056a36663d1e26a0f123a3832b92
Dan Ruscoe <danruscoe@gmail.com>
2014-08-31 22:51:55 -0700

    Added avatar loading service.

---
+++ Assets/Scripts/Services/AvatarService.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+
+public class AvatarService : WebService
+{
+	public void GetActiveAvatar()
+	{
+		Debug.Log("Getting user's active avatar.");
+
+		this.MakeRequest(server + "drpg/data/avatar");
+	}
+	
+	protected override void HandleSuccess(WWW webRequest)
+	{
+		Debug.Log(webRequest.text);
+		
+		AvatarServiceResponseModel response = JsonMapper.ToObject<AvatarServiceResponseModel>(webRequest.text);
+		
+		if (response.success)
+		{
+			Debug.Log("Got avatar.");
+			
+			base.HandleSuccess(webRequest);
+		}
+		else
+		{
+			this.failureResponseHandler(webRequest);
+		}
+	}
+	
+	protected override void HandleFailure(WWW webRequest)
+	{
+		base.HandleFailure(webRequest);
+		
+		Debug.Log(webRequest.text);
+		
+		Debug.LogError("Failed to get avatar.");
+	}
+}
---
+++ Assets/Scripts/Services/Models/AvatarServiceResponseModel.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public class AvatarServiceResponseModel : WebServiceResponseModel
+{
+	public AvatarModel avatar { get; set; }
+}
