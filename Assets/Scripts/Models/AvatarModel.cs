dd7477f811643ef3e99b0139a35eebb3229833f6
Dan Ruscoe <danruscoe@gmail.com>
2014-08-31 22:50:42 -0700

    Populated Avatar model.

--- Assets/Scripts/Models/AvatarModel.cs
+++ Assets/Scripts/Models/AvatarModel.cs
@@ -1,7 +1,11 @@
 ﻿public class AvatarModel : DataModel
 {
-	public int avatarId { get; set; }
+	public int avatar_id { get; set; }
+	public int owner_uid { get; set; }
 	public string label { get; set; }
 	public int xp { get; set; }
 	public int hp { get; set; }
+	public int created { get; set; }
+	public int updated { get; set; }
+	public object rdf_mapping { get; set; }
 }
