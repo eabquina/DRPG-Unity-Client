d32cf7879ef5aaf06a818f2f559a4e14daf6c8ee
Dan Ruscoe <danruscoe@gmail.com>
2014-09-01 23:59:37 -0700

    Updates to player stats now trigger update event.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,10 +3,47 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	private PlayerMovement playerMovement;
+	public delegate void UpdateEventHandler(object sender, PlayerEventArgs e);
 
-	void Awake()
+	public event UpdateEventHandler UpdateEvent;
+
+	private int xp;
+	private int hp;
+
+	public virtual int XP
 	{
-		this.playerMovement = (PlayerMovement) this.gameObject.GetComponent("PlayerMovement");
+		get { return this.xp; }
+		set {
+			this.xp = value;
+			
+			this.RaiseUpdateEvent();
+		}
 	}
+
+	public virtual int HP
+	{
+		get { return this.hp; }
+		set {
+			this.hp = value;
+				
+				this.RaiseUpdateEvent();
+		}
+	}
+
+	protected virtual void RaiseUpdateEvent()
+	{
+		Debug.Log("Updated player stats. XP: " + this.xp + ", HP: " + this.hp);
+
+		if (UpdateEvent != null)
+		{
+			UpdateEvent(this, new PlayerEventArgs(this.xp, this.hp));
+		}
+	}
+}
+
+public class PlayerEventArgs
+{
+	public PlayerEventArgs(int xp, int hp) { XP = xp; HP = hp; }
+	public int XP {get; private set;}
+	public int HP {get; private set;}
 }
