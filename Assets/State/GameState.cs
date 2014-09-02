09d44f87b2db4992a5311b28dde5d993000c2d23
Dan Ruscoe <danruscoe@gmail.com>
2014-09-02 00:03:17 -0700

    Added game state script.

---
+++ Assets/State/GameState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+
+public class GameState : MonoBehaviour
+{
+	Player player;
+
+	void Awake()
+	{
+		this.player = (Player) GameObject.FindGameObjectWithTag("Player").GetComponent("Player");
+
+		if (PlayerPrefs.HasKey("avatar"))
+		{
+			Debug.Log(PlayerPrefs.GetString("avatar"));
+
+			AvatarModel avatar = JsonMapper.ToObject<AvatarModel>(PlayerPrefs.GetString("avatar"));
+
+			if (avatar != null)
+			{
+				this.player.XP = avatar.xp;
+				this.player.HP = avatar.hp;
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Started game with no avatar data.");
+		}
+	}
+}
