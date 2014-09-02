2a2f53875f4f2686c3dc79814c0da7dcd6f07acf
Dan Ruscoe <danruscoe@gmail.com>
2014-09-02 00:35:04 -0700

    Added player name to update event.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,10 +7,21 @@ public class Player : MonoBehaviour
 
 	public event UpdateEventHandler UpdateEvent;
 
+	private string name;
 	private int xp;
 	private int hp;
 
-	public virtual int XP
+	public string Name
+	{
+		get { return this.name; }
+		set {
+			this.name = value;
+			
+			this.RaiseUpdateEvent();
+		}
+	}
+
+	public int XP
 	{
 		get { return this.xp; }
 		set {
@@ -20,7 +31,7 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	public virtual int HP
+	public int HP
 	{
 		get { return this.hp; }
 		set {
@@ -36,14 +47,15 @@ public class Player : MonoBehaviour
 
 		if (UpdateEvent != null)
 		{
-			UpdateEvent(this, new PlayerEventArgs(this.xp, this.hp));
+			UpdateEvent(this, new PlayerEventArgs(this.name, this.xp, this.hp));
 		}
 	}
 }
 
 public class PlayerEventArgs
 {
-	public PlayerEventArgs(int xp, int hp) { XP = xp; HP = hp; }
+	public PlayerEventArgs(string name, int xp, int hp) { Name = name; XP = xp; HP = hp; }
+	public string Name {get; private set;}
 	public int XP {get; private set;}
 	public int HP {get; private set;}
 }
