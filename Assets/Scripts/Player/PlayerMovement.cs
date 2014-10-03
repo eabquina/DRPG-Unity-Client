1df42fcfd82296a33ca8785bb3fda30a1bc3c2cb
Dan Ruscoe <danruscoe@gmail.com>
2014-10-03 00:37:11 -0700

    Added simple prop interaction.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,26 +6,42 @@ public class PlayerMovement : MonoBehaviour
 	private float speed = 2f;
 	private Vector3 target;
 
-	private int raycastLayerMask;
+	private int groundRaycastLayerMask;
+	private int propsRaycastLayerMask;
 	private RaycastHit raycastHit;
+
+	private InteractiveProp currentProp;
 
 	void Awake()
 	{
-		this.raycastLayerMask = 1 << LayerMask.NameToLayer("Ground");
+		this.groundRaycastLayerMask = 1 << LayerMask.NameToLayer("Ground");
+		this.propsRaycastLayerMask = 1 << LayerMask.NameToLayer("Props");
 	}
 	
 	void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
+			this.currentProp = null;
+
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (Physics.Raycast(ray, out this.raycastHit, Mathf.Infinity, this.raycastLayerMask))
+			if (Physics.Raycast(ray, out this.raycastHit, Mathf.Infinity, this.groundRaycastLayerMask))
 			{
 				Debug.Log("Clicked Ground at point:");
 				Debug.Log(this.raycastHit.point);
 
 				this.SetNewMoveTarget(new Vector3(this.raycastHit.point.x, this.gameObject.transform.position.y, this.raycastHit.point.z));
+			}
+			else if (Physics.Raycast(ray, out this.raycastHit, Mathf.Infinity, this.propsRaycastLayerMask))
+			{
+				this.currentProp = (InteractiveProp) this.raycastHit.collider.gameObject.GetComponent("InteractiveProp");
+
+				if (this.currentProp != null)
+				{
+					Debug.Log("Clicked prop: " + this.currentProp.gameObject.name);
+					this.currentProp.HandleInteraction();
+				}
 			}
 		}
 
