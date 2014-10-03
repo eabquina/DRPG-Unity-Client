c097726aa7b5b258e62cf241939801702480168f
Dan Ruscoe <danruscoe@gmail.com>
2014-10-03 00:24:21 -0700

    Started on prop scripts.

---
+++ Assets/Scripts/Props/InteractiveProp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveProp : MonoBehaviour
+{
+	public virtual void HandleInteraction()
+	{
+		Debug.Log("Interacted with prop: " + this.gameObject.name);
+	}
+}
---
+++ Assets/Scripts/Props/PortalProp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalProp : InteractiveProp
+{
+	public string targetScene;
+
+	public override void HandleInteraction()
+	{
+		if (this.targetScene.Length > 0)
+		{
+			Application.LoadLevel(this.targetScene);
+		}
+	}
+}
