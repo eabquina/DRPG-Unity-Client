4298db5addeac1fecdffd159574938225e5b0829
Dan Ruscoe <danruscoe@gmail.com>
2014-10-05 05:04:56 -0700

    Added functionality to populate rooms with item containers.

--- Assets/State/GameState.cs
+++ Assets/State/GameState.cs
@@ -5,17 +5,22 @@ using LitJson;
 
 public class GameState : MonoBehaviour
 {
+	public int roomId;
+	public ContainerProp containerPrefab;
+
 	private Player player;
 
 	private List<EventModel> events = new List<EventModel>();
 
 	private EventService eventService;
+	private RoomService roomService;
 
 	void Awake()
 	{
 		this.player = (Player) GameObject.FindGameObjectWithTag("Player").GetComponent("Player");
 
 		this.eventService = (EventService) this.gameObject.GetComponent("EventService");
+		this.roomService = (RoomService) this.gameObject.GetComponent("RoomService");
 
 		if (PlayerPrefs.HasKey("avatar"))
 		{
@@ -34,8 +39,14 @@ public class GameState : MonoBehaviour
 		{
 			Debug.LogWarning("Started game with no avatar data.");
 		}
+
+		if (this.roomId != 0)
+		{
+			this.roomService.successResponseHandler = new WebService.SuccessResponseHandler(this.PopulateRoomContainers);
+			this.roomService.GetRoom(this.roomId);
+		}
 	}
-	
+
 	public void CreateEvent(string method, List<string> arguments)
 	{
 		EventModel eventModel = new EventModel();
@@ -43,5 +54,41 @@ public class GameState : MonoBehaviour
 		eventModel.arguments = arguments;
 
 		this.events.Add(eventModel);
+	}
+
+	public void PopulateRoomContainers(WWW webRequest)
+	{
+		RoomModel room = JsonMapper.ToObject<RoomModel>(PlayerPrefs.GetString("room"));
+
+		if (room != null)
+		{
+			GameObject[] containerNodes = GameObject.FindGameObjectsWithTag("ContainerNode");
+
+			if (containerNodes.Length > 0)
+			{
+				ContainerProp containerProp = null;
+
+				for (int i = 0; i < containerNodes.Length; i++)
+				{
+					if (i < room.item_containers.Length)
+					{
+						// Instantiate new Item Container Prop.
+						containerProp = (ContainerProp) Instantiate(this.containerPrefab,
+							containerNodes[i].transform.position,
+							this.containerPrefab.transform.rotation);
+
+						containerProp.itemContainer = room.item_containers[i];
+					}
+				}
+			}
+			else
+			{
+				Debug.Log("Room contains no item containers.");
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Started game with no room date for room ID: " + this.roomId);
+		}
 	}
 }
