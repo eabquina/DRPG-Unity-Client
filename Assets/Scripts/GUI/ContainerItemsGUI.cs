1c27cf7b05d214493cde2f034f3c126c35562cd6
Dan Ruscoe <danruscoe@gmail.com>
2014-10-05 22:35:03 -0700

    Added container / item GUI scripts.

---
+++ Assets/Scripts/GUI/ContainerItemsGUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ContainerItemsGUI : MonoBehaviour
+{
+	public ItemSlotGUI itemSlotPrefab;
+
+	private List<ItemSlotGUI> itemSlots = new List<ItemSlotGUI>();
+
+	public void SetItems(ItemModel[] items)
+	{
+		this.itemSlots.Clear();
+
+		for (int i = 0; i < items.Length; i++)
+		{
+			this.CreateItemSlot(items[i]);
+		}
+	}
+
+	private void CreateItemSlot(ItemModel item)
+	{
+		ItemSlotGUI itemSlot = (ItemSlotGUI) Instantiate(this.itemSlotPrefab,
+			this.itemSlotPrefab.transform.position,
+			this.itemSlotPrefab.transform.rotation);
+		
+		itemSlot.SetItem(item);
+
+		this.itemSlots.Add(itemSlot);
+	}
+}
---
+++ Assets/Scripts/GUI/ItemSlotGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSlotGUI : MonoBehaviour
+{
+	private ItemModel item;
+
+	public void SetItem(ItemModel item)
+	{
+		Debug.Log("Setting Item Slot GUI item: " + item.label);
+	}
+}
