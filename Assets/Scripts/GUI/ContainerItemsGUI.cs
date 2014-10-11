3278c47ff41e7b36874e2c7fe336291157e462c8
Dan Ruscoe <danruscoe@gmail.com>
2014-10-11 01:20:02 -0700

    Fixed item slot offset.

--- Assets/Scripts/GUI/ContainerItemsGUI.cs
+++ Assets/Scripts/GUI/ContainerItemsGUI.cs
@@ -6,6 +6,8 @@ public class ContainerItemsGUI : MonoBehaviour
 {
 	public ItemSlotGUI itemSlotPrefab;
 
+	public float itemSlotPadding = 0.7f;
+	
 	private List<ItemSlotGUI> itemSlots = new List<ItemSlotGUI>();
 
 	public void SetItems(ItemModel[] items)
@@ -14,14 +16,15 @@ public class ContainerItemsGUI : MonoBehaviour
 
 		for (int i = 0; i < items.Length; i++)
 		{
-			this.CreateItemSlot(items[i]);
+			this.CreateItemSlot(items[i], i);
 		}
 	}
 
-	private void CreateItemSlot(ItemModel item)
+	private void CreateItemSlot(ItemModel item, int count)
 	{
+		float yOffset = (this.itemSlotPadding * (float)count);
 		ItemSlotGUI itemSlot = (ItemSlotGUI) Instantiate(this.itemSlotPrefab,
-			this.itemSlotPrefab.transform.position - new Vector3(0, 0, 1),
+			this.itemSlotPrefab.transform.position - new Vector3(0, yOffset, 1),
 			this.itemSlotPrefab.transform.rotation);
 
 		itemSlot.gameObject.transform.parent = this.gameObject.transform;
