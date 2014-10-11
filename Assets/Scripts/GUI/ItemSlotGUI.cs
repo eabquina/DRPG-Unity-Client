2f48b1b82f1bc22cdfb2bf751beda78c838fa990
Dan Ruscoe <danruscoe@gmail.com>
2014-10-11 01:07:33 -0700

    Item slots now display item name.

--- Assets/Scripts/GUI/ItemSlotGUI.cs
+++ Assets/Scripts/GUI/ItemSlotGUI.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class ItemSlotGUI : MonoBehaviour
 {
+	public TextMesh nameLabel;
+
 	private ItemModel item;
 
 	public void SetItem(ItemModel item)
 	{
 		Debug.Log("Setting Item Slot GUI item: " + item.label);
+
+		this.nameLabel.text = item.label;
 	}
 }
