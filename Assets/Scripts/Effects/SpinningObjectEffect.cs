83f9c8824bd25a1fd0e2f40540295c6b5c965d53
Dan Ruscoe <danruscoe@gmail.com>
2014-10-18 18:04:39 -0700

    Added effects scripts.

---
+++ Assets/Scripts/Effects/SpinningObjectEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningObjectEffect : MonoBehaviour
+{
+	public float rotateSpeed = 10.0f;
+
+	void Update()
+	{
+		transform.Rotate(0, 0, (this.rotateSpeed * Time.deltaTime));
+	}
+}
