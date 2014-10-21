b68484e17d67ccc1fd74189cf3f0b52e91994df3
Dan Ruscoe <danruscoe@gmail.com>
2014-10-20 22:57:38 -0700

    Added save button script.

---
+++ Assets/Scripts/GUI/SaveButtonGUI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveButtonGUI : GUI
+{
+	public Sprite enabledSprite;
+	public Sprite disabledSprite;
+
+	public GameState gameState;
+
+	private SpriteRenderer spriteRenderer;
+
+	private bool buttonEnabled;
+
+	void Awake()
+	{
+		this.spriteRenderer = (SpriteRenderer) this.gameObject.GetComponent("SpriteRenderer");
+
+		if (this.gameState != null)
+		{
+			this.gameState.UpdateEvent += this.HandleUpdate;
+		}
+	}
+
+	public void HandleUpdate(object sender)
+	{
+		this.SetEnabled();
+	}
+
+	public void HandleSelected()
+	{
+		if (buttonEnabled)
+		{
+			this.gameState.SendEvents();
+
+			this.SetDisabled();
+		}
+	}
+
+	private void SetEnabled()
+	{
+		this.spriteRenderer.sprite = this.enabledSprite;
+		this.buttonEnabled = true;
+	}
+
+	private void SetDisabled()
+	{
+		this.spriteRenderer.sprite = this.disabledSprite;
+		this.buttonEnabled = false;
+	}
+}
