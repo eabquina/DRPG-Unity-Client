aefd0c68d82c95c5b3cbafd397cd7cdddfc6ffc3
Dan Ruscoe <danruscoe@gmail.com>
2014-10-20 22:14:07 -0700

    Cursor script.

---
+++ Assets/Scripts/Cursor/Cursor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour
+{
+	public Camera uiCamera;
+	
+	private int raycastLayerMask;
+	
+	private RaycastHit2D raycastHit;
+	
+	private GUI currentGUI;
+	
+	void Awake()
+	{
+		Screen.showCursor = false;
+		
+		this.raycastLayerMask = 1 << LayerMask.NameToLayer("UI");
+	}
+	
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			this.currentGUI = null;
+
+			Vector3 cursorPoint = this.uiCamera.ScreenToWorldPoint(Input.mousePosition);
+
+			this.raycastHit = Physics2D.Raycast(cursorPoint, Vector2.up, 1f, this.raycastLayerMask);
+
+			if (this.raycastHit.collider != null)
+			{
+				this.currentGUI = (GUI) this.raycastHit.collider.gameObject.GetComponent("GUI");
+			}
+
+			if (this.currentGUI != null)
+			{
+				Debug.Log("Selected GUI item: " + this.currentGUI.gameObject.name);
+
+				this.currentGUI.SendMessage("HandleSelected");
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 cursorPoint = this.uiCamera.ScreenToWorldPoint(Input.mousePosition);
+		
+		this.transform.position = new Vector3(
+			cursorPoint.x,
+			cursorPoint.y,
+			this.transform.position.z);
+	}
+}
