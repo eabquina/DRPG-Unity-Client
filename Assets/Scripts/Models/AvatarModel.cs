71ab77aceaba437f0fa1ee169d0241076a0988ac
Dan Ruscoe <danruscoe@gmail.com>
2014-10-30 00:54:58 -0700

    Added function to get attribute values from avatar.

--- Assets/Scripts/Models/AvatarModel.cs
+++ Assets/Scripts/Models/AvatarModel.cs
@@ -5,4 +5,23 @@
 	public string label { get; set; }
 	public int currency { get; set; }
 	public AttributeModel[] attributes { get; set; }
+
+	public int GetAttributeValue(string name)
+	{
+		if (this.attributes != null)
+		{
+			AttributeModel attribute = null;
+			for (int i = 0; i < this.attributes.Length; i++)
+			{
+				attribute = this.attributes[i];
+
+				if (attribute.attribute_name == name)
+				{
+					return attribute.value;
+				}
+			}
+		}
+
+		return 0;
+	}
 }
