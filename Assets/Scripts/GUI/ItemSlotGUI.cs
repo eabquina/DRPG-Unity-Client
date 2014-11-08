31e51ed354c044d1a7c96d2a6e5ea2d906b4e711
Dan Ruscoe <danruscoe@gmail.com>
2014-11-08 11:34:41 -0800

    Made item slots clickable.

--- Assets/Scripts/GUI/ItemSlotGUI.cs
+++ Assets/Scripts/GUI/ItemSlotGUI.cs
@@ -1,16 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ItemSlotGUI : MonoBehaviour
+public class ItemSlotGUI : GUI
 {
 	public TextMesh nameLabel;
-
+	
 	private ItemModel item;
+
+	private GameState gameState;
+
+	void Awake()
+	{
+		GameObject gameState = GameObject.FindGameObjectWithTag("GameState");
+		this.gameState = (GameState) gameState.GetComponent("GameState");
+	}
 
 	public void SetItem(ItemModel item)
 	{
 		Debug.Log("Setting Item Slot GUI item: " + item.label);
 
-		this.nameLabel.text = item.label;
+		this.item = item;
+
+		this.nameLabel.text = this.item.label;
+	}
+
+	public void HandleSelected()
+	{
+		this.gameState.player.AddInventoryItem(this.item);
 	}
 }
