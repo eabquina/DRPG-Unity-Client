d7e84a50e8285dd2f38bd3a93b5b1e6a92357a69
Dan Ruscoe <danruscoe@gmail.com>
2014-11-08 15:17:10 -0800

    Spawned title objects no longer live forever.

--- Assets/Scripts/Title/TitleLaunchedObject.cs
+++ Assets/Scripts/Title/TitleLaunchedObject.cs
@@ -3,10 +3,24 @@ using System.Collections;
 
 public class TitleLaunchedObject : MonoBehaviour
 {
+	private float lifespan = 3.0f;
+
+	private float tDelta;
+
 	void Start()
 	{
 		float forceX = (this.transform.position.x > 0)? -150 : 150;
 
 		this.rigidbody.AddForce(forceX, Random.Range(250, 500), 0);
+	}
+
+	void Update()
+	{
+		this.tDelta += Time.deltaTime;
+
+		if (this.tDelta >= this.lifespan)
+		{
+			GameObject.Destroy(this.gameObject);
+		}
 	}
 }
