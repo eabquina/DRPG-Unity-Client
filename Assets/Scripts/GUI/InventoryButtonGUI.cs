585fc1010c30718388ef72ad02198c252b730c7a
Dan Ruscoe <danruscoe@gmail.com>
2014-11-08 10:54:51 -0800

    Inventory button now opens player inventory UI.

--- Assets/Scripts/GUI/InventoryButtonGUI.cs
+++ Assets/Scripts/GUI/InventoryButtonGUI.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class InventoryButtonGUI : GUI
 {
 	public GameState gameState;
 
+	public ContainerItemsGUI guiPrefab;
+
 	public void HandleSelected()
 	{
+		ContainerItemsGUI gui = (ContainerItemsGUI) Instantiate(this.guiPrefab,
+			this.guiPrefab.transform.position,
+			this.guiPrefab.transform.rotation);
 
+		List<ItemModel> inventoryItems = this.gameState.player.GetInventoryItems();
+
+		if (inventoryItems.Count != 0)
+		{
+			gui.SetItems(inventoryItems.ToArray());
+		}
 	}
 }
