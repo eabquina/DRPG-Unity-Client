a0761915bbe3970b0bd8cb8224a638dca3608af3
Dan Ruscoe <danruscoe@gmail.com>
2014-11-13 21:57:43 -0800

    Added inventory models.

---
+++ Assets/Scripts/Models/InventoryItemModel.cs
@@ -0,0 +1,7 @@
+﻿public class InventoryItemModel : DataModel
+{
+	public int inventory_item_id { get; set; }
+	public string type { get; set; }
+	public int entity_id { get; set; }
+	public int item_id { get; set; }
+}
---
+++ Assets/Scripts/Models/InventoryModel.cs
@@ -0,0 +1,5 @@
+﻿public class InventoryModel : DataModel
+{
+	public ItemModel[] items { get; set; }
+	public InventoryItemModel[] item_instances { get; set; }
+}
