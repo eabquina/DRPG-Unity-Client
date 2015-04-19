c42fdf77aa522819fe96044bfce7e8139cab0b27
Dan Ruscoe <danruscoe@gmail.com>
2015-04-19 15:14:52 -0700

    Added new item properties to match current DRPG module.

--- Assets/Scripts/Models/InventoryItemModel.cs
+++ Assets/Scripts/Models/InventoryItemModel.cs
@@ -3,5 +3,6 @@
 	public int inventory_item_id { get; set; }
 	public string type { get; set; }
 	public int entity_id { get; set; }
+	public int equipped { get; set; }
 	public int item_id { get; set; }
 }
--- Assets/Scripts/Models/ItemModel.cs
+++ Assets/Scripts/Models/ItemModel.cs
@@ -4,4 +4,8 @@
 	public string label { get; set; }
 	public int rarity { get; set; }
 	public int equippable { get; set; }
+	public int currency_cost { get; set; }
+	public int currency_value { get; set; }
+	public string ui_asset_path { get; set; }
+	public string game_asset_path { get; set; }
 }
